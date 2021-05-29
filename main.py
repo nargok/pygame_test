766d3e32a331b610067603e773654060f6369956
nargok <nargok@gmail.com>
2021-05-29 22:25:21 +0900

    display player

--- main.py
+++ main.py
@@ -5,15 +5,23 @@ import sys
 def main():
     # print("hello pygame!")
     pygame.init()
-    screen = pygame.display.set_mode((500, 637))
+    (w, h) = (500, 637)
+    (x, y) = (w/2, h/2)
+
+    pygame.display.set_mode((w, h), 0, 32)
+    screen = pygame.display.get_surface()
 
     pygame.display.set_caption("Pygame Test")
     bg = pygame.image.load("akasaka.jpg").convert_alpha()
     rect_bg = bg.get_rect()
+    player = pygame.image.load("yuusya_game.png").convert_alpha()
+    rect_player = player.get_rect()
+    rect_player.center = (x, y)
 
     while(True):
         screen.fill((0, 0, 0, 0))
         screen.blit(bg, rect_bg)
+        screen.blit(player, rect_player)
         pygame.time.wait(30)
         pygame.display.update()
 
